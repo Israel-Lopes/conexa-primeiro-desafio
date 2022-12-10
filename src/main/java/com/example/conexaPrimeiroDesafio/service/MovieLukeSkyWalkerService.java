dde8e36fd59f1452b091ca473e9c479351d91a8a
Israel-Lopes <yisrael.lopes@gmail.com>
2022-12-09 22:50:51 -0300

    refatorado

--- src/main/java/com/example/conexaPrimeiroDesafio/service/MovieLukeSkyWalkerService.java
+++ src/main/java/com/example/conexaPrimeiroDesafio/service/MovieLukeSkyWalkerService.java
@@ -3,6 +3,7 @@ package com.example.conexaPrimeiroDesafio.service;
 import com.example.conexaPrimeiroDesafio.integration.FilmCatalogIntegration;
 import com.example.conexaPrimeiroDesafio.integration.PeopleCatalogIntegration;
 import com.example.conexaPrimeiroDesafio.model.Film;
+import com.example.conexaPrimeiroDesafio.model.People;
 import com.example.conexaPrimeiroDesafio.model.catalog.PeopleCatalog;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,6 +12,7 @@ import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class MovieLukeSkyWalkerService {
@@ -23,18 +25,15 @@ public class MovieLukeSkyWalkerService {
     private PeopleCatalogIntegration peopleIntegration;
 
     public List<Film> getMoviesSkyWalker() {
-
-        ArrayList<String> moviesSky = new ArrayList<>();
         PeopleCatalog peoples = peopleIntegration.getPeople();
-        List<Film> skys = new ArrayList<>();
+        List<String> moviesSky = new ArrayList<>();
 
-        for (int i = 0; i < peoples.getResults().size(); i++) {
-            if (peoples.getResults().get(i).getName().equals(NAME)) {
+        for (People people : peoples.getResults()) {
+            if (people.getName().equals(NAME)) {
                 log.info("Parametros de busca para people: {}", NAME);
-                for (int j = 0 ; j < peoples.getResults().get(i).getFilms().size(); j++) {
-                    String id = peoples.getResults().get(i).getFilms().get(j).substring(28, 29);
-                    moviesSky.add(j, id.toString());
-                    log.info("Parametros de busca para films: {}", id.toString());
+                for (String filmUrl : people.getFilms()) {
+                    moviesSky.add(filmUrl.substring(28, 29));
+                    log.info("Parametros de busca para films: {}", filmUrl.substring(28, 29));
                 }
                 break;
             }
@@ -46,12 +45,8 @@ public class MovieLukeSkyWalkerService {
             return null;
         }
 
-        for (int i = 0; i < moviesSky.size(); i++) {
-            Integer id = Integer.parseInt(moviesSky.get(i));
-            log.info("Parametros de busca para films: {}", id.toString());
-            skys.add(i, filmIntegration.getFilmById(id));
-        }
-
-        return skys;
+        return moviesSky.stream()
+                .map(id -> filmIntegration.getFilmById(Integer.parseInt(id)))
+                .collect(Collectors.toList());
     }
 }
